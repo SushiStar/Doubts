8502430b8370d73b13abc7e6710958eb16e8dd23
w <wei@dexterity.ai>
2021-09-08 11:05:18 -0700

    feat: eigen translate and then rotate

--- src/eigen.cpp
+++ src/eigen.cpp
@@ -8,24 +8,54 @@
 
 // STL include
 #include <array>
+#include <cassert>
 #include <iostream>
 
 // external include
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
+/*
+ * int main() {
+ *   //  x, y, z, w
+ *   std::array<double, 4> arr{0.3, 0.27, 0, 1.0};
+ *   Eigen::Vector4d vct{0.3, 0.27, 0, 1};
+ *
+ *   // Eigen::Quaterniond quat1(vct(0), vct(1), vct(2), vct(3));
+ *   // Eigen::Quaterniond quat2(arr[0], arr[1], arr[2], arr[3]);
+ *   Eigen::Quaterniond quat1(vct.data());
+ *   Eigen::Quaterniond quat2(arr.data());
+ *
+ *   printf("should be: %.2f %.2f %.2f %.2f\n", arr[0], arr[1], arr[2], arr[3]);
+ *   printf("%.2f %.2f %.2f %.2f\n", quat1.x(), quat1.y(), quat1.z(),
+ * quat1.w()); printf("%.2f %.2f %.2f %.2f\n", quat2.x(), quat2.y(), quat2.z(),
+ * quat2.w()); return 0;
+ * }
+ */
+
+/*
+ * int main() {
+ *   Eigen::VectorXd v1(4);
+ *   Eigen::Vector3d v2{0.1, 0.2, 0.3};
+ *
+ *   v1.head(3) = v2;
+ *   v1(3) = 0.8;
+ *   std::cout << v1 << std::endl;
+ *   return 0;
+ * }
+ */
+
 int main() {
-  //  x, y, z, w
-  std::array<double, 4> arr{0.3, 0.27, 0, 1.0};
-  Eigen::Vector4d vct{0.3, 0.27, 0, 1};
+  Eigen::Vector2d tran{0.2, 0.3};
+  Eigen::Rotation2Dd ori(0.785398);
 
-  // Eigen::Quaterniond quat1(vct(0), vct(1), vct(2), vct(3));
-  // Eigen::Quaterniond quat2(arr[0], arr[1], arr[2], arr[3]);
-  Eigen::Quaterniond quat1(vct.data());
-  Eigen::Quaterniond quat2(arr.data());
+  Eigen::Affine2d pose;
+  pose.setIdentity();
+  pose.translate(tran);
+  pose.rotate(ori);
 
-  printf("should be: %.2f %.2f %.2f %.2f\n", arr[0], arr[1], arr[2], arr[3]);
-  printf("%.2f %.2f %.2f %.2f\n", quat1.x(), quat1.y(), quat1.z(), quat1.w());
-  printf("%.2f %.2f %.2f %.2f\n", quat2.x(), quat2.y(), quat2.z(), quat2.w());
+  Eigen::Vector2d origin{0.1, 0.0};
+  Eigen::Vector2d newpt = pose * origin;
+  std::cout << newpt << std::endl;
   return 0;
 }
