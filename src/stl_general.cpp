2d637cb9d59b4bf58b7d466d8344dfe4065658e3
wei du <wei@dexterity.ai>
2021-11-17 14:51:40 -0800

    fix: update

--- src/stl_general.cpp
+++ src/stl_general.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
+/*
+ * int main() {
+ *   std::vector<int> v1{1, 2, 3, 4, 5, 6, 7};
+ *   std::vector<int> v2{8, 9};
+ *
+ *   v2.insert(v2.end(), v1.begin() + 1, v1.begin() + 2);
+ *   for (auto i : v2) {
+ *     std::cout << i << ", ";
+ *   }
+ *   printf("\n");
+ *
+ *   return 0;
+ * }
+ */
+
+/*
+ * int main() {
+ *   std::unordered_map<std::string, uint32_t> mp;
+ *   std::cout << mp["asldfkj"] << std::endl;
+ *   std::cout << mp["asldkfja"] << std::endl;
+ * }
+ */
+
+// conclusion: lambda capture is const by default
 int main() {
-  std::vector<int> v1{1, 2, 3, 4, 5, 6, 7};
-  std::vector<int> v2{8, 9};
-
-  v2.insert(v2.end(), v1.begin() + 1, v1.begin() + 2);
-  for (auto i : v2) {
-    std::cout << i << ", ";
-  }
-  printf("\n");
-
-  return 0;
+  const std::string test{"sldfkj"};
+  auto lmd = [&test](int idx) -> std::string {
+    return test + "2" + std::to_string(idx);
+  };
+  std::cout << lmd(3) << std::endl;
 }
