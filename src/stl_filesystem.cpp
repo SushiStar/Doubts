2d6bfc28ebde8f19ca972f5ed2d399e731e30d96
w <wei@dexterity.ai>
2022-03-17 22:40:26 -0700

    filesystem

--- src/stl_filesystem.cpp
+++ src/stl_filesystem.cpp
@@ -25,19 +25,39 @@
 //   };
 // }
 
+/*
+ * int main() {
+ *   std::string filepath{"/home/w/Downloads/cat_driver.log"};
+ *   //{"timestamp":"2021-11-09T11:10:29.137453-08:00",
+ *   //"message":"pressure_reading",
+ *   //"extra":{"cup":5,"error_delay":0,"pressure":0.0,"state":1}}
+ *   const char fmt[] = "{\"%[^\"]\":\"%[^\"]\", \"%[^\"]\":\"%[^\"]\", %s}";
+ *   std::fstream fs(filepath);
+ *   std::string buffer;
+ *   char timestamp[10], stamp[50], message[50], msg[50], tmp[4096];
+ *   while (std::getline(fs, buffer)) {
+ *     std::sscanf(buffer.c_str(), fmt, timestamp, stamp, message, msg, tmp);
+ *     std::cout << stamp << " " << msg << " " << tmp << std::endl;
+ *   }
+ *
+ *   return 0;
+ * }
+ */
+// {"timestamp":"2021-11-09T11:10:29.136801-08:00",
+// "message":"pressure_reading",
+// "extra":{"cup":0,"error_delay":0,"pressure":0.0,"state":1}}
+
 int main() {
-  std::string filepath{"/home/w/Downloads/cat_driver.log"};
-  //{"timestamp":"2021-11-09T11:10:29.137453-08:00",
-  //"message":"pressure_reading",
-  //"extra":{"cup":5,"error_delay":0,"pressure":0.0,"state":1}}
-  const char fmt[] = "{\"%[^\"]\":\"%[^\"]\", \"%[^\"]\":\"%[^\"]\", %s}";
-  std::fstream fs(filepath);
-  std::string buffer;
-  char timestamp[10], stamp[50], message[50], msg[50], tmp[4096];
-  while (std::getline(fs, buffer)) {
-    std::sscanf(buffer.c_str(), fmt, timestamp, stamp, message, msg, tmp);
-    std::cout << stamp << " " << msg << " " << tmp << std::endl;
+  std::string filedir{"/home/w/Downloads"};
+  char fmt[] = "{\"timestamp\":\"%s\", \"message\":\"%s\", %s}";
+  std::ifstream fs(filedir);
+  if (!fs.is_open())
+    std::cout << "Cannot open file: " << filedir << std::endl;
+
+  for (std::string line; std::getline(fs, line);) {
+    std::cout << line << std::endl;
   }
 
+  fs.close();
   return 0;
 }
